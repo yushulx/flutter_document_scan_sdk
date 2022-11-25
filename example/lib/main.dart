4e1fb3ca3580fc16b8055aafc67bfeb3fc7ecb6d
yushulx <lingxiao1002@gmail.com>
2022-11-25 09:38:46 +0800

    Update example UI

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,8 +1,15 @@
+import 'dart:html';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'dart:async';
 
 import 'package:flutter/services.dart';
 import 'package:flutter_document_scan_sdk/flutter_document_scan_sdk.dart';
+
+import 'dart:ui' as ui;
+
+import 'package:image_picker/image_picker.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,14 +22,46 @@ class MyApp extends StatefulWidget {
   State<MyApp> createState() => _MyAppState();
 }
 
+class ImagePainter extends CustomPainter {
+  ImagePainter(this.image, this.points);
+  final ui.Image image;
+  final List<Offset> points;
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint()
+      ..color = Colors.red
+      ..strokeWidth = 5
+      ..style = PaintingStyle.stroke;
+
+    canvas.drawImage(image, Offset.zero, paint);
+    canvas.drawLine(Offset(126, 27), Offset(733, 65), paint);
+    canvas.drawLine(Offset(733, 65), Offset(715, 989), paint);
+    canvas.drawLine(Offset(715, 989), Offset(20, 934), paint);
+    canvas.drawLine(Offset(20, 934), Offset(126, 27), paint);
+  }
+
+  @override
+  bool shouldRepaint(ImagePainter oldDelegate) =>
+      image != oldDelegate.image || points != oldDelegate.points;
+}
+
 class _MyAppState extends State<MyApp> {
   String _platformVersion = 'Unknown';
   final _flutterDocumentScanSdkPlugin = FlutterDocumentScanSdk();
+  final picker = ImagePicker();
+  String file = '';
+  late ui.Image image;
 
   @override
   void initState() {
     super.initState();
     initPlatformState();
+  }
+
+  Future<ui.Image> loadImage(XFile file) async {
+    final data = await file.readAsBytes();
+    return await decodeImageFromList(data);
   }
 
   // Platform messages are asynchronous, so we initialize in an async method.
@@ -32,7 +71,8 @@ class _MyAppState extends State<MyApp> {
     // We also handle the message potentially returning null.
     try {
       platformVersion =
-          await _flutterDocumentScanSdkPlugin.getPlatformVersion() ?? 'Unknown platform version';
+          await _flutterDocumentScanSdkPlugin.getPlatformVersion() ??
+              'Unknown platform version';
     } on PlatformException {
       platformVersion = 'Failed to get platform version.';
     }
@@ -47,6 +87,16 @@ class _MyAppState extends State<MyApp> {
     });
   }
 
+  Widget createCustomImage(ui.Image image) {
+    return SizedBox(
+      width: image.width.toDouble(),
+      height: image.height.toDouble(),
+      child: CustomPaint(
+        painter: ImagePainter(image, []),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -54,9 +104,50 @@ class _MyAppState extends State<MyApp> {
         appBar: AppBar(
           title: const Text('Plugin example app'),
         ),
-        body: Center(
-          child: Text('Running on: $_platformVersion\n'),
-        ),
+        body: Stack(children: <Widget>[
+          Center(
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  file == ''
+                      ? Image.asset('images/default.png')
+                      : createCustomImage(image),
+                ],
+              ),
+            ),
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Container(
+                height: 100,
+                child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      MaterialButton(
+                          textColor: Colors.white,
+                          color: Colors.blue,
+                          onPressed: () async {
+                            final pickedFile = await picker.pickImage(
+                                source: ImageSource.camera);
+                            if (pickedFile != null) {
+                              image = await loadImage(pickedFile);
+                              setState(() {
+                                file = pickedFile.path;
+                              });
+                            }
+                          },
+                          child: const Text('Load Document')),
+                      MaterialButton(
+                          textColor: Colors.white,
+                          color: Colors.blue,
+                          onPressed: () async {},
+                          child: const Text("Save Document"))
+                    ]),
+              ),
+            ],
+          )
+        ]),
       ),
     );
   }
